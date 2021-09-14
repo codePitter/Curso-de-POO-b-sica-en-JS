c09a01bded68a652c03e7ff04048dada62190f94
Pitter <pitterbck@gmail.com>
2021-09-14 01:09:53 -0300

    Shallow copy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const juan = {
+/*const juan = {
     name: "Juanito",
     age: 18,
     approvesCourses: ["Curso 1"],
@@ -11,44 +11,45 @@ const juan = {
 };
 
 //para agregar un curso podríamos hacer lo siguiente
-//juan.addCourse("Curso 2");
+juan.addCourse("Curso 2");
 
-// console.log(Object.keys(juan));
-// console.log(Object.getOwnPropertyNames(juan));
-// console.log(Object.entries(juan));
+console.log(Object.keys(juan));
+console.log(Object.getOwnPropertyNames(juan));
+console.log(Object.entries(juan));
 
-// Object.defineProperty(juan, "navigator", {
-//     value: "Chrome",
-//     enumerable: false,
-//     writable: true,
-//     configurable: true,
-// });
+Object.defineProperty(juan, "navigator", {
+     value: "Chrome",
+     enumerable: false,
+     writable: true,
+     configurable: true,
+});
 
-// Object.defineProperty(juan, "pruebaNasa", {
-//     value: "Extraterrestres",
-//     enumerable: false,
-//     writable: false,
-//     configurable: false,
-// });
+Object.defineProperty(juan, "pruebaNasa", {
+     value: "Extraterrestres",
+     enumerable: false,
+     writable: false,
+     configurable: false,
+});
 
-// Object.defineProperty(juan, "editor", {
-//     value: "VS Code",
-//     enumerable: true,
-//     writable: false,
-//     configurable: true,
-// });
+Object.defineProperty(juan, "editor", {
+     value: "VS Code",
+     enumerable: true,
+     writable: false,
+     configurable: true,
+});
 
-// Object.defineProperty(juan, "terminal", {
-//     value: "Windows",
-//     enumerable: true,
-//     writable: true,
-//     configurable: false,
-// });
+Object.defineProperty(juan, "terminal", {
+     value: "Windows",
+     enumerable: true,
+     writable: true,
+     configurable: false,
+});
 
-//Object.seal(juan);// cambia la propiedad canfigurable a false lo que no deja que se borre
+Object.seal(juan);// cambia la propiedad canfigurable a false lo que no deja que se borre
 Object.freeze(juan);// no deja que se edite ni se borre
 
 console.log(Object.getOwnPropertyDescriptors(juan));
 
 console.log(Object.getOwnPropertyDescriptors(juan));
 console.log(juan);
+*/
